041e4f8a2313e11566b7097bad17d754242f3681
Lukas Steinhauser <L.Steinhauser@printcom.ch>
2020-01-28 21:58:13 +0100

    Innitialer Commit für das gewünschte .gitignore und die Grundbausteine des Projektes

--- src/model/Frage.java
+++ src/model/Frage.java
@@ -1,5 +1,10 @@
 package model;
 
+import java.util.Map;
+
 public interface Frage {
 
+    public String getText();
+    public Map<Integer, String> getMultipleChoices();
+    public int getCorrectAnswer();
 }
