89ec7d38392e9955e46e5a649a0f4e36d1b3afba
Lukas Steinhauser <L.Steinhauser@printcom.ch>
2020-01-28 21:54:27 +0100

    Innitialer Commit für das gewünschte .gitignore und die Grundbausteine des Projektes

---
+++ src/data/FragenBO.java
@@ -0,0 +1,15 @@
+package data;
+
+import model.Frage;
+
+import java.util.Collections;
+import java.util.List;
+
+public class FragenBO {
+
+
+    public List<Frage> getFragenliste(){
+        //TODO Method STUB
+        return Collections.emptyList();
+    }
+}
