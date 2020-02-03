59210d1832c23abf43a2942f95122afc428a6ccb
grighoma <60393126+grighoma@users.noreply.github.com>
2020-02-03 20:30:20 +0100

    category Java new 10 questions and 2 typs of questions

--- src/data/TestData.java
+++ src/data/TestData.java
@@ -27,7 +27,16 @@ public class TestData {
         Category java = new Category();
         java.setName("Java");
         java.add(new Question("Which of the below is valid way to instantiate an array in java? \n 1:int myArray [] = {1, 3, 5}; \n 2:int myArray [] [] = {1,2,3,4};\n 3:int [] myArray = {\"1\", \"2\", \"3\"};"  ,"1"));
-        java.add(new Question("Which of the below are reserved keyword in java?\n 1:null \n 2:array \n 3:int", "3"));
+        java.add(new Question("Which of the following statements is true?\n 1:Java has a huge devoloper community \n 2:Java is used only in NASA's space related applications \n 3:Java is used only in web and mobile applications", "1"));
+        java.add(new Question("Which method is the starting point for all Java programs?","main" ));
+        java.add(new Question("Fill the blank to declare a method called: hello() \n void _____()","hello"));
+        java.add(new Question("Which method prints text in a Java program?\n1: out.writeText() \n2: System.out.println()\n3: System.printText()","2"));
+        java.add(new Question("Sigle-line comments are created using:", "//"));
+        java.add(new Question("Which variable type wold you use for a city name?", "String"));
+        java.add(new Question(" What is the result of the following code ? \nint x =15;inty=4;\nint result=x/y;\nSystem.out.println(result);","3"));
+        java.add(new Question("What value is stored in the result variable? \nint x =8;inty=5;\nint result=x%y;\nSystem.out.println(result);","3"));
+        java.add(new Question("Fill the blank to print 11.\nint a=10;\n___a;\nSystem.out.println(a);","++"));
+
         result.add(java);
         return result;
     }
