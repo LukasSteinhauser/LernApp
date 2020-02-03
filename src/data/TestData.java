71cff43f91b7576032925dde0dff365950f5a1ab
grighoma <60393126+grighoma@users.noreply.github.com>
2020-02-03 13:24:56 +0100

    Merge branch 'master' of C:\Users\Stefano\Documents\GitHub\LernApp with conflicts.

--- src/data/TestData.java
+++ src/data/TestData.java
@@ -8,22 +8,25 @@ import java.util.List;
 
 public class TestData {
     public static List<Category> getTestCategories(){
-        Category category = new Category();
-        category.setName("Hauptstädte");
-        category.add(new Question("Wie heißt die Hauptstadt von Deutschland?","Berlin"));
-        category.add(new Question("Wie heißt die Hauptstadt von den Niederlanden?","Amsterdam"));
-        category.add(new Question("Wie heißt die Hauptstadt von der Schweiz?","Bern"));
-        category.add(new Question("Wie heißt die Hauptstadt von Syrien?","Damaskus"));
-        category.add(new Question("Wie heißt die Hauptstadt von Griechenland?","Athen"));
-        category.add(new Question("Wie heißt die Hauptstadt von Frankreich?","Paris"));
-        category.add(new Question("Wie heißt die Hauptstadt von China?","Peking"));
-        category.add(new Question("Wie heißt die Hauptstadt von den USA?","Washington D.C."));
-        category.add(new Question("Wie heißt die Hauptstadt von Russland?","Moskau"));
-        category.add(new Question("Wie heißt die Hauptstadt von Großbritannien?","London"));
+        Category hauptstaedte = new Category();
+        hauptstaedte.setName("Hauptstädte");
+        hauptstaedte.add(new Question("Wie heißt die Hauptstatt von Deutschland?","Berlin"));
+        hauptstaedte.add(new Question("Wie heißt die Hauptstatt von den Niederlanden?","Amsterdam"));
+        hauptstaedte.add(new Question("Wie heißt die Hauptstatt von der Schweiz?","Bern"));
+        hauptstaedte.add(new Question("Wie heißt die Hauptstatt von Syrien?","Damaskus"));
+        hauptstaedte.add(new Question("Wie heißt die Hauptstatt von Griechenland?","Athen"));
+        hauptstaedte.add(new Question("Wie heißt die Hauptstatt von Frankreich?","Paris"));
+        hauptstaedte.add(new Question("Wie heißt die Hauptstatt von China?","Peking"));
+        hauptstaedte.add(new Question("Wie heißt die Hauptstatt von den USA?","Washington D.C."));
+        hauptstaedte.add(new Question("Wie heißt die Hauptstatt von Russland?","Moskau"));
+        hauptstaedte.add(new Question("Wie heißt die Hauptstatt von Großbritannien?","London"));
 
         var result = new ArrayList<Category>();
-        result.add(category);
-
+        result.add(hauptstaedte);
+        Category java = new Category();
+        java.setName("Java");
+        java.add(new Question("Which of the below is valid way to instantiate an array in java? \n 1:int myArray [] = {1, 3, 5}; \n 2:int myArray [] [] = {1,2,3,4};\n 3:int [] myArray = {\"1\", \"2\", \"3\"};"  ,"Berlin"));
+        result.add(java);
         return result;
     }
 }
