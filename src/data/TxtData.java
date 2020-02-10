6b70966a2dee06b334097e2ca6a8768ce25717c9
GhaithDmirieh <60393141+GhaithDmirieh@users.noreply.github.com>
2020-02-10 21:16:22 +0100

    Moglichkeit, um Fragen zu einer Vom User erstellten Kategorie hinzufuegen.

--- src/data/TxtData.java
+++ src/data/TxtData.java
@@ -15,38 +15,39 @@ import java.util.stream.Collectors;
 
 public class TxtData {
 
-    private static String basicPath = System.getenv("APPDATA")+ File.separator
-            +"LernApp"+File.separator+"Data"+File.separator;
+    private static String basicPath = System.getenv("APPDATA") + File.separator
+            + "LernApp" + File.separator + "Data" + File.separator;
 
-    private static String getCategorieFilePath(Category category){
+    private static String getCategorieFilePath(Category category) {
         return basicPath + category.getName() + ".txt";
     }
 
-    public  static void deleteCategory (Category category, String bestaetigung){
+    public static void deleteCategory(Category category, String bestaetigung) {
 
         var diesCategory = getCategorieFilePath(category);
-        var diesesFile = new  File(diesCategory);
+        var diesesFile = new File(diesCategory);
 
         final boolean meineKategorien = category.getName().equals("Java") && category.getName().equals("Haupstädte");
 
-        if (bestaetigung.equals("ja".toLowerCase()) && !meineKategorien){
+        if (bestaetigung.equals("ja".toLowerCase()) && !meineKategorien) {
             diesesFile.delete();
         }
     }
 
-    public static void editCategory (Category category){
+    public static void editCategory(Category category) {
 
         final boolean meineKategorien = category.getName().equals("Java") && category.getName().equals("Haupstädte");
 
         if (!meineKategorien) {
             System.out.println("Category umbennen ? 1 eingeben.");
             System.out.println("Fragen bearbeiten ? 2 eingeben.");
+            System.out.println("Frage hinzufügen ? 3 eingeben.");
             int auswahl = Scan.nextInt();
 
 
-            if (auswahl == 1){
+            if (auswahl == 1) {
 
-                deleteCategory(category,"ja");
+                deleteCategory(category, "ja");
                 System.out.print("Wie lautet deinen gewünschten Namen: ");
                 String newName = Scan.nextLine();
 
@@ -54,9 +55,9 @@ public class TxtData {
                 saveCategory(category);
             }
 
-            if(auswahl == 2){
+            if (auswahl == 2) {
 
-                for( int j = 0 ;  j < category.size(); j++){
+                for (int j = 0; j < category.size(); j++) {
                     Question question = category.get(j);
 
                     System.out.println("Hier ist die Frage: \n");
@@ -65,109 +66,172 @@ public class TxtData {
                     System.out.println("Bearbeite die Frage ? tippe \"1\" \nDie Frage beibehalten ? tippe  \"2\" \nDie Frage löschen ? tippe \"3\" ");
                     int abfrage = Scan.nextInt();
 
-                    if(abfrage == 1){
-                        EditCategory.editFragen(question ,category, j);
+                    if (abfrage == 1) {
+                        EditCategory.editFragen(question, category, j);
                     }
 
-                    if(abfrage == 2){
-                        category.set(j,question);
+                    if (abfrage == 2) {
+                        category.set(j, question);
                     }
 
-                    if(abfrage == 3){
+                    if (abfrage == 3) {
                         category.remove(j);
                         j--;
                         saveCategory(category);
                     }
                 }
             }
+
+            if (auswahl == 3) {
+                boolean wiederhole = true;
+                while (wiederhole) {
+
+
+                    System.out.println("Für Frage mit freier Antwort  \"1\" tippen \nFür multiple choice Frage \"2\" tippen");
+                    int artDerFrage = Scan.nextInt();
+
+                    if (artDerFrage == 1) {
+
+                        System.out.print("Deine Frage: ");
+                        String neueFrage = Scan.nextLine();
+
+                        System.out.print("Deine Antwort: ");
+                        String antwort = Scan.nextLine();
+
+                        Question question = new Question(antwort, neueFrage);
+
+                        category.add(question);
+                        saveCategory(category);
+
+                    }
+
+                    if (artDerFrage == 2) {
+
+                        System.out.print("Deine Frage: ");
+                        String neueFrage = Scan.nextLine();
+
+                        System.out.print("Wie viele mögliche Antworten werden vorgestellt ? ");
+                        int elementenAnzahl = Scan.nextInt();
+                        String frage[] = new String[elementenAnzahl + 1];
+
+                        frage[0] = neueFrage;
+
+                        for (int i = 1; i <= elementenAnzahl; i++) {
+
+                            System.out.print("Auswahl Nr. " + i + ' ');
+                            String eingabe = Scan.nextLine();
+                            frage[i] = eingabe;
+                        }
+
+                        System.out.print("Deine Antwort: ");
+                        String antwort = Scan.nextLine();
+
+                        Question question = new Question(antwort, frage);
+
+                        category.add(question);
+                        saveCategory(category);
+                    }
+
+                    System.out.println("\nWeitere Fragen hinzufügen ? 1 Tippen\nFertig ? 2 Tippen");
+                    int fertig = Scan.nextInt();
+
+                    if(fertig == 1){
+                        System.out.println("okay");
+                    }
+                    if(fertig == 2){
+                        wiederhole = false;
+                    }
+                }
+            }
         }
     }
 
-    public static void saveCategory(Category category){
+    public static void saveCategory(Category category) {
         var path = getCategorieFilePath(category);
 
-        try{
+        try {
             File file = new File(path);
 
             file.getParentFile().mkdirs();
 
             PrintWriter writer = new PrintWriter(file);
 
-            for (var question: category) {
-               writer.println(question.getAntwort());
-               writer.println(question.getFrage());
-               writer.println();
+            for (var question : category) {
+                writer.println(question.getAntwort());
+                writer.println(question.getFrage());
+                writer.println();
             }
 
             writer.close();
-        }catch (IOException ex){
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 
-    public static List<Category> loadAllCategories(){
+    public static List<Category> loadAllCategories() {
         File dir = new File(basicPath);
 
-        if(dir.exists()&&dir.exists()){
+        if (dir.exists() && dir.exists()) {
             var files = new ArrayList<File>(Arrays.asList(dir.listFiles()))
                     .stream().filter(file -> {
-                String name = file.getName();
+                        String name = file.getName();
 
-                if(name.length()>=4&&".txt".equals(name.substring(name.length()-4,name.length()).toLowerCase())){
-                    return true;
-                }
-                return false;
-            }).collect(Collectors.toList());
+                        if (name.length() >= 4 && ".txt".equals(name.substring(name.length() - 4, name.length()).toLowerCase())) {
+                            return true;
+                        }
+                        return false;
+                    }).collect(Collectors.toList());
 
             var result = new ArrayList<Category>();
 
-            for (var file: files){
+            for (var file : files) {
                 Category tempCategory = new Category();
-                tempCategory.setName(file.getName().replace(".txt",""));
+                tempCategory.setName(file.getName().replace(".txt", ""));
 
-                try{
+                try {
                     BufferedReader reader = new BufferedReader(new FileReader(file));
 
                     boolean newQuestion = true;
                     Question tempQuestion = null;
                     var questionLines = new ArrayList<String>();
-                    for(String line = reader.readLine(); line != null ; line = reader.readLine()){
-                        if(line.isBlank()){
-                            if(tempQuestion!=null){
+                    for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                        if (line.isBlank()) {
+                            if (tempQuestion != null) {
                                 String[] fragenListe = questionLines.toArray(new String[questionLines.size()]);
 
                                 tempQuestion.setFrage(fragenListe);
                                 tempCategory.add(tempQuestion);
                                 questionLines = new ArrayList<String>();
-                                tempQuestion=null;
+                                tempQuestion = null;
                             }
                             newQuestion = true;
-                        }else if(newQuestion){
+                        } else if (newQuestion) {
                             tempQuestion = new Question();
                             tempQuestion.setAntwort(line);
                             newQuestion = false;
-                        }else{
+                        } else {
                             questionLines.add(line);
                         }
                     }
 
                     reader.close();
-                }catch(IOException ex){
+                } catch (IOException ex) {
                     ex.printStackTrace();
                 }
 
                 result.add(tempCategory);
             }
-        return result;
+            return result;
         }
 
         return Collections.emptyList();
     }
-    public static List<Category> initCategories(){
+
+    public static List<Category> initCategories() {
         var list = loadAllCategories();
-        if(list.size()==0){
+        if (list.size() == 0) {
             list = TestData.getTestCategories();
-            for(var category : list){
+            for (var category : list) {
                 saveCategory(category);
             }
         }
